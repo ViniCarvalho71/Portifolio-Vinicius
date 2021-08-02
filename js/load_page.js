6176c31e1331e68d23bd94ff63f54751c427b8dc
M0onFall <79428412+M0onFall@users.noreply.github.com>
2021-08-02 14:33:10 +0000

    Aula 02/08 - Aprimoramentos da modal

--- js/load_page.js
+++ js/load_page.js
@@ -5,7 +5,48 @@ $(document).ready(function(){
    $('a').click(function(e){
      e.preventDefault()
 
-     $('#modal-info').modal('show')
+     //Criar uma vairável local que receba o atributo href do link
+
+     let url = $(this).attr('href')
+
+     $('.modal-title').empty()
+     $('.modal-body').empty()
+
+     //Verificar qual conteúfo carregar
+
+     switch(url){
+    //Se for a pag sobre mim
+     case 'about':
+      $('.modal-title').append('Sobre mim')
+      $('.modal-body').append(`<p><strong>Nome</strong>: Vinícius Carvalho</p>
+      <p><strong>Desenvolvidor de Sistemas</strong></p>
+      <p><strong>Idade</strong>: 16 anos</p>
+      <p><strong>Curso</strong>: Desenvolvimento de Sistemas</p>
+      <p><strong>Módulo</strong>: 2° ano</p>`)
+      $('#modal-info').modal('show')
+      break
+     //Se for a pag trabalhos
+     case 'work': 
+      $('.modal-title').append('Trabalhos')
+      $('.modal-body').append('Lorem')
+      $('#modal-info').modal('show')
+      break
+     //Se for a pag parceiros
+     case 'client': 
+      $('.modal-title').append('Clientes')
+      $('.modal-body').append(`<strong>Cliente 1:</strong>`)
+      $('#modal-info').modal('show')
+      break
+     //Se for a pag contato
+     case 'contact': 
+      $('.modal-title').append('Contato')
+      $('.modal-body').append(`<p><strong>Email</strong>: carvalhovinicius71@gmail.com</p>
+      <p><strong>Linkedin</strong>: ----------</p>
+      <p><strong>Celular</strong>: 14 99616-4842</p>`)
+      $('#modal-info').modal('show')
+      break
+     default:alert('link não encontrado')
+     }
    })
 
 })
